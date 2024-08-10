96f9d1e5b718d1971807bb44e67474f5939b3c5b
Kjamm <happyminig@gmail.com>
2024-08-10 22:55:05 +0900

    feat : diversity.py

---
+++ microbiome_analysis/diversity.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy.stats import entropy
+
+def shannon_index(counts):
+    proportions = counts / np.sum(counts)
+    return entropy(proportions)
+
+def simpson_index(counts):
+    proportions = counts / np.sum(counts)
+    return 1 - np.sum(proportions**2)
+
+def calculate_alpha_diversity(data):
+    shannon = data.apply(shannon_index, axis=1)
+    simpson = data.apply(simpson_index, axis=1)
+    return pd.DataFrame({'shannon': shannon, 'simpson': simpson})
