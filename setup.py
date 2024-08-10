e2949a57bc0656077fdfdca5ab6e99d7251e67d0
Kjamm <happyminig@gmail.com>
2024-08-11 00:13:02 +0900

    feat : update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="microbiome_analysis",
     version="0.1.0",
     description="A comprehensive Python library for microbiome data analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Jaemin Kim",
     author_email="jaemin52577441@gmail.com",
     packages=find_packages(),
