61596cdcd33ad230724929270311978aba185d67
Kjamm <happyminig@gmail.com>
2024-08-11 00:41:57 +0900

    update

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="microbiome_analysis",
-    version="0.1.2",
+    version="0.1.3",
     description="A comprehensive Python library for microbiome data analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Jaemin Kim",
     author_email="jaemin52577441@gmail.com",
+    url="https://kjamm.github.io/microbiome_analysis/",  # GitHub Pages URL
+    project_urls={
+        "Documentation": "https://kjamm.github.io/microbiome_analysis/",  # GitHub Pages URL
+        "Source Code": "https://github.com/kjamm/microbiome_analysis",
+        "Issue Tracker": "https://github.com/kjamm/microbiome_analysis/issues",
+    },
     packages=find_packages(),
     install_requires=[
         "biopython",
