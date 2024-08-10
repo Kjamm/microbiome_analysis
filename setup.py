d59e68f6d4c98c39925cb0b3da74a9f6bf596129
Kjamm <happyminig@gmail.com>
2024-08-11 00:17:49 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="microbiome_analysis",
-    version="0.1.0",
+    version="0.1.1",
     description="A comprehensive Python library for microbiome data analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
