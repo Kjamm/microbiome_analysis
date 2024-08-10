f05ebe3b0472eb68e9d6a9af48a692d49c4bfdca
Kjamm <happyminig@gmail.com>
2024-08-10 23:02:09 +0900

    feat : visualization.py

---
+++ microbiome_analysis/visualization.py
@@ -0,0 +1,17 @@
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+def plot_alpha_diversity(diversity_df):
+    fig = px.box(diversity_df)
+    fig.show()
+
+def plot_pca(coords, labels):
+    plt.scatter(coords[:, 0], coords[:, 1], c=labels)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.title('PCA')
+    plt.show()
+
+def plot_beta_diversity(coords, method='PCoA'):
+    fig = px.scatter(coords, x=0, y=1, title=method)
+    fig.show()
